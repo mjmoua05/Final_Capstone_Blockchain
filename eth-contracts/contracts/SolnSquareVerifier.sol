adc537427cbc506742a1638c552377990c91f9c2
mjmoua05 <68444965+mjmoua05@users.noreply.github.com>
2020-10-15 06:31:37 -0700

    Update SolnSquareVerifier.sol

--- eth-contracts/contracts/SolnSquareVerifier.sol
+++ eth-contracts/contracts/SolnSquareVerifier.sol
@@ -1,6 +1,7 @@
 pragma solidity >=0.5.0;
 import "./ERC721Mintable.sol";
 import "./Verifier.sol";
+
 // TODO define a contract call to the zokrates generated solidity contract <Verifier> or <renamedVerifier>
 contract SquareVerifier is Verifier {
 
@@ -18,6 +19,7 @@ contract SolnSquareVerifier is CustomERC721Token {
   struct Solution {
     uint256 index;
     address to;
+    bytes32 hashKey;
   }
 
   // TODO define an array of the above struct
@@ -27,14 +29,14 @@ contract SolnSquareVerifier is CustomERC721Token {
   mapping(bytes32 => Solution) public uniqueSolutions;
 
   // TODO Create an event to emit when a solution is added
-  event SolutionAdded(address to, uint256 index);
+  event SolutionAdded(address to, uint256 index, bytes32 hashKey);
 
   // TODO Create a function to add the solutions to the array and emit the event
   function addSolution(uint[2] memory a, uint[2][2] memory b, uint[2] memory c, uint[2] memory input) public {
-        bytes32 key = keccak256(abi.encodePacked(a, b, c, input));
-        require(uniqueSolutions[key].index == 0, "Solution already exsits");
-        uniqueSolutions[key] = Solution({index: solCount, to: msg.sender});
-        solCount += 1;
+        bytes32 hash = keccak256(abi.encodePacked(a, b, c, input));
+        require(uniqueSolutions[hash].index == 0, "Solution already exsits");
+        uniqueSolutions[hash] = Solution({index: solCount, to: msg.sender, hashKey:key});
+        solCount+=1;
         emit SolutionAdded(msg.sender, solCount);
     }
 
@@ -42,13 +44,12 @@ contract SolnSquareVerifier is CustomERC721Token {
   //  - make sure the solution is unique (has not been used before)
   //  - make sure you handle metadata as well as tokenSupply
   function mintToken(address _to, uint256 _index, uint[2] memory a, uint[2][2] memory b, uint[2] memory c, uint[2] memory input) public {
-    bytes32 key = keccak256(abi.encodePacked(a, b, c, input));
-    require(uniqueSolutions[key].to == address(0), "Solution already exists!");
-    require(verifierContract.verifyTx(a, b, c, input), "Solution not correct!");
+    bytes32 hash = keccak256(abi.encodePacked(a, b, c, input));
+    require(uniqueSolutions[hash].to == address(0), "Solution already exists!");
     addSolution(a, b, c, input);
+    emit SolutionAdded(msg.sender, solCount);
     mint(_to, _index);
   }
-
-
+  
 
 }
